318c0e70b1cd4cc1a34cba2f7fd56b805b5a3b66
WiiruxCruz <hialf.92@gmail.com>
2022-07-18 02:28:08 -0500

    SQL logging

---
+++ src/main/java/com/wiirux/sdjpaintro/bootstrap/DataInitializer.java
@@ -0,0 +1,35 @@
+package com.wiirux.sdjpaintro.bootstrap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.wiirux.sdjpaintro.domain.Book;
+import com.wiirux.sdjpaintro.repositories.BookRepository;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+	
+	private final BookRepository br;
+	
+	public DataInitializer(BookRepository br) {
+		this.br = br;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
+		
+		System.out.println("ID:" + bookDDD.getId());
+		
+		Book savedDDD = br.save(bookDDD);
+		
+		Book bookSIA = new Book("Spring in Action", "234234", "Oreily");
+		Book savedSIA = br.save(bookSIA);
+		
+		br.findAll().forEach( book -> {
+			System.out.println("Book id:" + book.getId());
+			System.out.println("Book title:" + book.getTitle());
+			System.out.println("Book ISBN:" + book.getIsbn());
+		} );
+	}
+
+}
