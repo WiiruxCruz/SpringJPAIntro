b612b8418d39c5e5b0a0f184d71ad020b7956784
WiiruxCruz <hialf.92@gmail.com>
2022-07-20 01:16:40 -0500

    Add JUnit Test for JPA

--- src/test/java/com/wiirux/sdjpaintro/SdjpaIntroApplicationTests.java
+++ src/test/java/com/wiirux/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -1,10 +1,24 @@
 package com.wiirux.sdjpaintro;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.wiirux.sdjpaintro.repositories.BookRepository;
 
 @SpringBootTest
 class SdjpaIntroApplicationTests {
+	
+	@Autowired
+	BookRepository br;
+	
+	@Test
+	void testBookRepository() {
+		long count = br.count();
+		assertThat(count).isGreaterThan(0);
+	}
 
 	@Test
 	void contextLoads() {
