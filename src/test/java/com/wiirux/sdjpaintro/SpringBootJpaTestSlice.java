b4db0826fc83beb892ab2d417d02c19b797b8df2
WiiruxCruz <hialf.92@gmail.com>
2022-07-26 00:43:05 -0500

    Add compatibility mode to H2 in Mysql

--- src/test/java/com/wiirux/sdjpaintro/SpringBootJpaTestSlice.java
+++ src/test/java/com/wiirux/sdjpaintro/SpringBootJpaTestSlice.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
@@ -19,6 +20,7 @@ import com.wiirux.sdjpaintro.repositories.BookRepository;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 @ComponentScan( basePackages = {"com.wiirux.sdjpaintro.bootstrap"})
+@AutoConfigureTestDatabase( replace = AutoConfigureTestDatabase.Replace.NONE)
 class SpringBootJpaTestSlice {
 	
 	@Autowired
@@ -31,7 +33,7 @@ class SpringBootJpaTestSlice {
 	void testJPATestSplice() {
 		long countBefore = br.count();
 		
-		assertThat(countBefore).isEqualTo(0);
+		assertThat(countBefore).isEqualTo(2);
 		
 		br.save(new Book("My Book", "1235555", "Self"));
 		
@@ -45,7 +47,7 @@ class SpringBootJpaTestSlice {
 	void testJpaTestSpliceTransaction() {
 		long countBefore = br.count();
 		
-		assertThat(countBefore).isEqualTo(1);
+		assertThat(countBefore).isEqualTo(3);
 	}
 
 }
