cdb2da5bede4e05a6a1e6fc0f8ff0c437ca7b86a
WiiruxCruz <hialf.92@gmail.com>
2022-07-31 00:54:25 -0500

    Creación de tabla Author con flyway

---
+++ src/main/java/com/wiirux/sdjpaintro/domain/Author.java
@@ -0,0 +1,70 @@
+package com.wiirux.sdjpaintro.domain;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Author {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String first_name;
+	private String last_name;
+	
+	public Author() {
+		
+	}
+
+	public Author(String first_name, String last_name) {
+		this.first_name = first_name;
+		this.last_name = last_name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null || getClass() != obj.getClass()) return false;
+		
+		Author author = (Author) obj;
+		
+		//return id != null ? id.equals(book.id) : book.id == null;
+		return Objects.equals(id, author.id);
+	}
+	
+	@Override
+	public int hashCode() {
+		//return Objects.hash(id);
+		return id != null ? id.hashCode() : 0;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	
+	
+}
