1120412ab703aca31543e092d8876d423fe0db38
WiiruxCruz <hialf.92@gmail.com>
2022-08-02 00:35:39 -0500

    Auto increment primary keys

--- src/main/java/com/wiirux/sdjpaintro/domain/Author.java
+++ src/main/java/com/wiirux/sdjpaintro/domain/Author.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Author {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String first_name;
