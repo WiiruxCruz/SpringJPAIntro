b90ad385092631c4e527ca38ea2f19f20380f505
WiiruxCruz <hialf.92@gmail.com>
2022-08-11 02:09:32 -0500

    Creación de pruebas para los nuevos repository
    Repository de AuthorUuid y BookUuid

--- src/test/java/com/wiirux/sdjpaintro/MySQLIntegrationTest.java
+++ src/test/java/com/wiirux/sdjpaintro/MySQLIntegrationTest.java
@@ -11,7 +11,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.wiirux.sdjpaintro.domain.AuthorUuid;
+import com.wiirux.sdjpaintro.domain.BookUuid;
+import com.wiirux.sdjpaintro.repositories.AuthorUuidRepository;
 import com.wiirux.sdjpaintro.repositories.BookRepository;
+import com.wiirux.sdjpaintro.repositories.BookUuidRepository;
 
 @ActiveProfiles("local")
 @DataJpaTest
@@ -22,10 +26,37 @@ public class MySQLIntegrationTest {
 	@Autowired
 	BookRepository br;
 	
+	@Autowired
+	AuthorUuidRepository aur;
+	
+	@Autowired
+	BookUuidRepository bur;
+	
+	@Test
+	void testBookUuid() {
+		BookUuid bookUuid = bur.save(new BookUuid());
+		assertThat(bookUuid).isNotNull();
+		assertThat(bookUuid.getId());
+		
+		BookUuid fetched = bur.getById(bookUuid.getId());
+		assertThat(fetched).isNotNull();
+		
+		
+	}
+	
+	@Test
+	void testAuthorId() {
+		AuthorUuid authorUuid = aur.save(new AuthorUuid());
+		assertThat(authorUuid).isNotNull();
+		assertThat(authorUuid.getId()).isNotNull();
+		
+		AuthorUuid fetched = aur.getById(authorUuid.getId());
+		assertThat(fetched).isNotNull();
+	}
+	
 	@Test
 	void tetsMySQL() {
 		long countBefore = br.count();
-		
 		assertThat(countBefore).isEqualTo(2);
 	}
 }
