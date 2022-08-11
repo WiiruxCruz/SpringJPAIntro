7a9c05f3a2572bd79ebbca60e03b4336dd632cd2
WiiruxCruz <hialf.92@gmail.com>
2022-08-11 02:08:35 -0500

    Cambio al repository de bookuuid

--- src/main/java/com/wiirux/sdjpaintro/repositories/BookUuidRepository.java
+++ src/main/java/com/wiirux/sdjpaintro/repositories/BookUuidRepository.java
@@ -1,9 +1,11 @@
 package com.wiirux.sdjpaintro.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wiirux.sdjpaintro.domain.BookUuid;
 
-public interface BookUuidRepository extends JpaRepository<BookUuid, Long> {
+public interface BookUuidRepository extends JpaRepository<BookUuid, UUID> {
 
 }
